9fac1552cfe62855d0c691dacbfb35ad29e3cd68
Tamanji Che <cheambetamanji@gmail.com>
2019-11-11 10:53:12 +0200

    Express server app

---
+++ node2019/myapp/index.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hellow from my Node server!'));
+
+app.get('/demo', (req, res) => {
+    console.log('request', req);
+    res.send('demo')});
+
+app.listen(port, () => console.log(`server app started, listening on port ${port}!`));
