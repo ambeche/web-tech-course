38079fc6b9fe1b4084dce4054f85e42bcce89abe
Tamanji Che <cheambetamanji@gmail.com>
2019-11-15 12:59:31 +0200

    public directory added

--- node2019/myapp/index.js
+++ node2019/myapp/index.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+app.use(express.static('public'));
 
 app.get('/', (req, res) => res.send('Hellow from my Node server!'));
 
