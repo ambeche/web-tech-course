06319c9a70eedd997f498460a2fd1be622b2f5fd
Tamanji Che <cheambetamanji@gmail.com>
2019-11-21 14:16:50 +0200

    Connection to DB and SQL in separate module; body parser with HTTP POST and mysql INSERT

--- node2019/myapp/index.js
+++ node2019/myapp/index.js
@@ -1,32 +1,50 @@
 'use strict';
 
-
-//console.log('my powerful app.');
-const connection = require('./model/db.js');
-
 const express = require('express');
+const busstop = require('./model/busstop');
 
-const port = 3000;
 const app = express();
+const bodyParser = require('body-parser');
 
 app.use(express.static('public'));
 
 app.get('/busstop', async (req, res) => {
-    try{
-        const [results, fields] = await connection.query(
-                'SELECT * FROM busschedule.busstop');
+  try {
+    res.json(await busstop.getAll());
+  } catch (e) {
+    console.log(e);
+    res.send('db error :(');
+  }
+});
 
-                        console.log(results);
-                        console.log(fields);
-                        res.json(results);
-} catch (e) {
-        console.log(e);
-        res.send('db error :(');
-}
+app.get('/busstop', async (req, res) => {
+  console.log(req.query);
+  try {
+    res.json(await busstop.search(req.query.name));
+  } catch(e) {
+    res.send(`db error`);
+  }
+});
 
+app.post('/busstop', bodyParser.urlencoded({extended: true}), async (req, res) => {
+  console.log(req.body);
+  try {
+    res.json(await busstop.insert(req.body.name));
+  } catch (e) {
+    console.log(e);
+    res.send('db error');
+  }
+});
 
+app.get('/', (req, res) => {
+  res.send('Hello form my Node server');
+});
+
+app.get('/demo', (req, res) => {
+  console.log('request', req);
+  res.send('demo');
 });
 
 app.listen(3000, () => {
-	console.log('server app start?');
+  console.log('server app start?');
 });
