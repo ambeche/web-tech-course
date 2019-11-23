bf224521228b7f0b50d3e72c26483f1c67869322
Tamanji Che <cheambetamanji@gmail.com>
2019-11-23 18:11:39 +0200

    MVC implemented for user routes, and user's password deleted from sent data

--- node2019/week5/app.js
+++ node2019/week5/app.js
@@ -4,9 +4,9 @@ const app = express();
 const port = 3000;
 
 const catRoute = require('./routes/catRoute');
-//const userRoute = require('./routes/userRoute');
+const userRoute = require('./routes/userRoute');
 
 app.use('/cat', catRoute);
-//app.use('/user', userRoute);
+app.use('/user', userRoute);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
