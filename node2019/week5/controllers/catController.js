f2ff6a2f1760663bb20edff867a7a67e27e8a227
Tamanji Che <cheambetamanji@gmail.com>
2019-11-23 16:15:37 +0200

    Path variable passed to controller and req.param.id implemented to route to cat of given id

--- node2019/week5/controllers/catController.js
+++ node2019/week5/controllers/catController.js
@@ -6,7 +6,12 @@ const cats = catModel.cats;
 const cat_list_get = (req, res) => {
   res.json(cats);
 };
+
+const cat_get = (req, res) => {
+    res.json(cats.filter(val => val.id == req.params.id));
+};
    
 module.exports = {
   cat_list_get,
+  cat_get,
 };
--- node2019/week5/routes/catRoute.js
+++ node2019/week5/routes/catRoute.js
@@ -5,9 +5,7 @@ const catController = require('../controllers/catController')
 
 router.get('/', catController.cat_list_get);
 
-router.get('/:id', (req, res) => {
-  res.send('You reqested a cat whose id is ' + req.params.id);
-});
+router.get('/:id', catController.cat_get);
 
 router.post('/', (req, res) => {
   res.send('With this endpoint you can add cats.');
