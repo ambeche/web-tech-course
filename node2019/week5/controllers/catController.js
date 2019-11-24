ba7f346981e7b4dccbf304503e0dd71d6547de61
Tamanji Che <cheambetamanji@gmail.com>
2019-11-24 03:34:08 +0200

    connection to database, db.js added to model

--- node2019/week5/controllers/catController.js
+++ node2019/week5/controllers/catController.js
@@ -1,15 +1,18 @@
 'use strict';
 const catModel = require('../models/catModel');
 
-const cats = catModel.cats;
+//const cats = catModel.cats;
 
-const cat_list_get = (req, res) => {
-  res.json(cats);
-};
+const cat_list_get = async (req, res) => {
+    const cats = await catModel.getAllCats();
+    await res.json(cats);
+  };
 
-const cat_get = (req, res) => {
-    res.json(cats.filter(val => val.id == req.params.id));
-};
+  const cat_get = async (req, res) => {
+    const params = [req.params.id];
+    const cat = await catModel.getCat(params);
+    await res.json(cat);
+  };
 
 const cat_create_post = (req, res) => {
     res.send('With this endpoint you can add cats.');
